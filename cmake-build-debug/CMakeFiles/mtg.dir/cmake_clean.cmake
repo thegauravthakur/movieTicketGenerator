8769c55fb54457822e1ca358e8bf30a1151e6beb
Gaurav Thakur <gthakur581@gmail.com>
2019-04-18 18:17:06 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/mtg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtg.dir/main.cpp.o"
+  "mtg.pdb"
+  "mtg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mtg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
